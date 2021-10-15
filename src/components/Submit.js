74bf991ee675d40c64cc3f49229885da56b2819c
Myrteza <myrteza@hotmail.fr>
2021-10-15 12:31:02 +0200

    :html-css: add comments for a better understanding of the code

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -6,13 +6,13 @@ function Submit(props){
     return (
         <div className={props.className} >
 
-            <input /*a reetirer*/ id="inputEmail" onChange={props.onChange} className="submitInput" name="email" type="email" placeholder="Email Address" />
+            <input id="inputEmail" onChange={props.onChange} className="submitInput" name="email" type="email" placeholder="Email Address" />
 
             <div id="warningContainer" className={props.wrnCtnClassName} >
             </div>
 
             <button className="submitBtn" onClick={props.handleBtnClick}>
-                <img className="arrowI" src={arrowIcon} alt="icon fleche" />
+                <img className="arrow" src={arrowIcon} alt="icon fleche" />
             </button>
             
         </div>
